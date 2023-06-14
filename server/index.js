71811e09fb054958a7fd0b169f1e6a48bb59664c
Lucas Rothman <lrothman@mit.edu>
2023-06-13 23:47:22 -0400

    added express middleware

--- server/index.js
+++ server/index.js
@@ -1,14 +1,27 @@
 import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 app.use(express.static("../client/index.js"));
 
+// Helmet
+app.use(
+    helmet({
+        contentSecurityPolicy: false,
+        crossOriginEmbedderPolicy: false,
+    })
+);
+
 // Body Parser
 app.use(bodyParser.json());
+
+// Cookie Parser
+app.use(cookieParser());
 
 // API
 import api from "./api/api.js";
