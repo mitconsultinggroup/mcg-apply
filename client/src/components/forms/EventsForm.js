72699689bc62bae923eccaaa010535e41ea0fb0a
emchen125 <99142788+emchen125@users.noreply.github.com>
2024-02-01 11:53:41 -0500

    dates and stuffs

--- client/src/components/forms/EventsForm.js
+++ client/src/components/forms/EventsForm.js
@@ -9,33 +9,40 @@ export default function EventsForm(usertype) {
     const events = [{
         id: "meettheteam",
         name: "Meet the Team",
-        timelocation: "8 - 9:30pm | September 11th | Room 2-190",
+        timelocation: "8 - 9:30pm | February 12th | Room 56-154",
         description: "Meet current members of MCG and learn more about MCG's recruitment process during our first recruitment event of the semester."
+    },
+    {
+        id: "pdpanel",
+        name: "Professional Development Panel",
+        timelocation: "7 - 8:30pm | February 13th | Room 2-105",
+        description: "Hear from current members about their experiences in various industries and the role MCG has played in developing their careers."
     },
     {
         id: "deipanel",
         name: "DEI Panel",
-        timelocation: "7 - 8:30pm | September 12th | Room 4-270",
-        description: " Talk to current women and BIPOC within MCG. Hear about their experiences and MCG's efforts to create a more inclusive environment."
+        timelocation: "7 - 8:30pm | February 14th | Room 2-105",
+        description: "Hear from current women and BIPOC within MCG about their professional experiences and MCG's efforts to create a more inclusive environment."
     },
     {
         id: "resumereview",
         name: "Resume Review",
-        timelocation: " 7 - 9pm | September 13th | Room 6-120",
+        timelocation: " 7 - 9pm | February 15th | Room 4-270",
         description: "Come work on your resume with the help of current MCG consultants who have landed offers at top consulting, finance, and tech companies."
     },
     {
         id: "cheesecakesocial",
         name: "Cheesecake Social",
-        timelocation: "7:30 - 9pm | September 14th | By Invitation",
+        timelocation: "7:30 - 9pm | February 16th | By Invitation",
         description: "Join us for a night of cheesecake and games and learn more about the MCG community, social events, and the work our members do in a casual setting."
     },
     {
         id: "caseworkshop",
         name: "Case Workshop",
-        timelocation: "12:30 - 2pm | September 16th | By Invitation",
+        timelocation: "12:30 - 2pm | February 17th | By Invitation",
         description: "Learn more about consulting and prepare for MCG interviews by working through a mock case with MCG consultants."
-    }]
+    }
+]
     
     return (
         <div className="flex flex-wrap gap-8 px-10 justify-center">
