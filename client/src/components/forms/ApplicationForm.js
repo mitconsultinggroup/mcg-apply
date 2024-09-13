d210c8e926a22173c213720359d00b37b4a233af
Emma Chen <emmachen@mit.edu>
2024-09-13 00:09:53 -0400

    close app

--- client/src/components/forms/ApplicationForm.js
+++ client/src/components/forms/ApplicationForm.js
@@ -12,6 +12,7 @@ export default function ApplicationForm() {
     const [opt1, setOpt1] = useState("");
     const [error, setError] = useState("");
     const [submitted, setSubmitted] = useState(false);
+    const [open, setopen] = useState(false);
 
     useEffect(() => {
         fetch("/api/application/get-application")
@@ -111,141 +112,154 @@ export default function ApplicationForm() {
     };
 
     return (
-        <div className="space-y-4 md:space-y-6">
+        !open ? <div className="space-y-4 md:space-y-6">
             <div>
                 <h3>
-                    You can submit your application as many times as you'd like before the
-                    application deadline. We will only consider your most recent
-                    submission. Be sure to press the submit button at the bottom of the
-                    page when you are done or want to save your answers!
+                    Our application for the Fall 2024 cycle is closed.
                     <br></br>
                     <br></br>
-                    If you have any technical difficulties,
+                    If you have any questions or concerns,
                     please reach out to Emma Chen (
                     <a className="hover:text-blue-600" href="mailto:emmachen@mit.edu">emmachen@mit.edu</a>).
                 </h3>
             </div>
-            <div>
-                <label
-                    className="block mb-2 text-sm font-medium text-gray-900"
-                >
-                    First Name
-                </label>
-                <input
-                    type="text"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                    value={firstname}
-                    onChange={(e) => {
-                        setError("");
-                        setFirstname(e.target.value);
-                    }}
-                />
-            </div>
-            <div>
-                <label
-                    className="block mb-2 text-sm font-medium text-gray-900"
-                >
-                    Last Name
-                </label>
-                <input
-                    type="text"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                    value={lastname}
-                    onChange={(e) => {
-                        setError("");
-                        setLastname(e.target.value);
-                    }}
-                />
-            </div>
-            <div>
-                <label
-                    className="block mb-2 text-sm font-medium text-gray-900"
-                >
-                    Your email
-                </label>
-                <input
-                    type="email"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                    placeholder="kerb@mit.edu"
-                    value={email}
-                    onChange={(e) => {
-                        setError("");
-                        setEmail(e.target.value);
-                    }}
-                />
-            </div>
-            <div>
-                <label
-                    className="block mb-2 text-sm font-medium text-gray-900"
-                >
-                    Class Year
-                </label>
-                <input
-                    placeholder="Ex: 2027"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                    value={classYear}
-                    onChange={(e) => {
-                        setError("");
-                        setClassYear(e.target.value);
-                    }}
-                />
-            </div>
-            <div>
-                <label
-                    className="block mb-2 text-sm font-medium text-gray-900"
-                >
-                    Profile Image
-                </label>
-                <input
-                    className="block w-full text-sm font-medium text-gray-900 border border-gray-300 rounded-lg p-1.5 cursor-pointer bg-gray-50 focus:outline-none"
-                    type="file"
-                    accept="image/*"
-                    onChange={(e) => {
-                        setError("");
-                        handleProfileUpload(e.target.files[0]);
-                    }}
-                />
-            </div>
-
-            <div>
-                <label className="block mb-2 text-sm font-medium text-gray-900">
-                    Resume Upload
-                </label>
-                <input
-                    className="block w-full text-sm font-medium text-gray-900 border border-gray-300 rounded-lg p-1.5 cursor-pointer bg-gray-50 focus:outline-none"
-                    type="file"
-                    accept=".pdf,.doc,.docx"
-                    onChange={(e) => {
-                        setError("");
-                        handleResumeUpload(e.target.files[0]);
-                    }}
-                />
-            </div>
-
-            <div>
-                <label
-                    className="block mb-2 text-sm font-medium text-gray-900"
-                >
-                    Optional: What do you hope to gain from MCG?
-                </label>
-                <textarea
-                    className="p-2 resize-none bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full h-48"
-                    value={opt1}
-                    onChange={(e) => {
-                        setError("");
-                        setOpt1(e.target.value);
-                    }}
-                />
-            </div>
-
-            <button
-                onClick={submitApplication}
-                className="text-sm h-10 px-5 text-white transition-colors duration-150 bg-red-700 rounded-lg focus:shadow-outline hover:bg-red-800"
-            >
-                Submit Application
-            </button>
-            {submitted ? <p className="text-green-500">Submitted successfully!</p> : ""}
-            <p className="text-red-500">{error.capitalize()}</p>
         </div>
+            :
+            <div className="space-y-4 md:space-y-6">
+                <div>
+                    <h3>
+                        You can submit your application as many times as you'd like before the
+                        application deadline. We will only consider your most recent
+                        submission. Be sure to press the submit button at the bottom of the
+                        page when you are done or want to save your answers!
+                        <br></br>
+                        <br></br>
+                        If you have any technical difficulties,
+                        please reach out to Emma Chen (
+                        <a className="hover:text-blue-600" href="mailto:emmachen@mit.edu">emmachen@mit.edu</a>).
+                    </h3>
+                </div>
+                <div>
+                    <label
+                        className="block mb-2 text-sm font-medium text-gray-900"
+                    >
+                        First Name
+                    </label>
+                    <input
+                        type="text"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                        value={firstname}
+                        onChange={(e) => {
+                            setError("");
+                            setFirstname(e.target.value);
+                        }}
+                    />
+                </div>
+                <div>
+                    <label
+                        className="block mb-2 text-sm font-medium text-gray-900"
+                    >
+                        Last Name
+                    </label>
+                    <input
+                        type="text"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                        value={lastname}
+                        onChange={(e) => {
+                            setError("");
+                            setLastname(e.target.value);
+                        }}
+                    />
+                </div>
+                <div>
+                    <label
+                        className="block mb-2 text-sm font-medium text-gray-900"
+                    >
+                        Your email
+                    </label>
+                    <input
+                        type="email"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                        placeholder="kerb@mit.edu"
+                        value={email}
+                        onChange={(e) => {
+                            setError("");
+                            setEmail(e.target.value);
+                        }}
+                    />
+                </div>
+                <div>
+                    <label
+                        className="block mb-2 text-sm font-medium text-gray-900"
+                    >
+                        Class Year
+                    </label>
+                    <input
+                        placeholder="Ex: 2027"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                        value={classYear}
+                        onChange={(e) => {
+                            setError("");
+                            setClassYear(e.target.value);
+                        }}
+                    />
+                </div>
+                <div>
+                    <label
+                        className="block mb-2 text-sm font-medium text-gray-900"
+                    >
+                        Profile Image
+                    </label>
+                    <input
+                        className="block w-full text-sm font-medium text-gray-900 border border-gray-300 rounded-lg p-1.5 cursor-pointer bg-gray-50 focus:outline-none"
+                        type="file"
+                        accept="image/*"
+                        onChange={(e) => {
+                            setError("");
+                            handleProfileUpload(e.target.files[0]);
+                        }}
+                    />
+                </div>
+
+                <div>
+                    <label className="block mb-2 text-sm font-medium text-gray-900">
+                        Resume Upload
+                    </label>
+                    <input
+                        className="block w-full text-sm font-medium text-gray-900 border border-gray-300 rounded-lg p-1.5 cursor-pointer bg-gray-50 focus:outline-none"
+                        type="file"
+                        accept=".pdf,.doc,.docx"
+                        onChange={(e) => {
+                            setError("");
+                            handleResumeUpload(e.target.files[0]);
+                        }}
+                    />
+                </div>
+
+                <div>
+                    <label
+                        className="block mb-2 text-sm font-medium text-gray-900"
+                    >
+                        Optional: What do you hope to gain from MCG?
+                    </label>
+                    <textarea
+                        className="p-2 resize-none bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full h-48"
+                        value={opt1}
+                        onChange={(e) => {
+                            setError("");
+                            setOpt1(e.target.value);
+                        }}
+                    />
+                </div>
+
+                <button
+                    onClick={submitApplication}
+                    className="text-sm h-10 px-5 text-white transition-colors duration-150 bg-red-700 rounded-lg focus:shadow-outline hover:bg-red-800"
+                >
+                    Submit Application
+                </button>
+                {submitted ? <p className="text-green-500">Submitted successfully!</p> : ""}
+                <p className="text-red-500">{error.capitalize()}</p>
+            </div>
     );
 }
