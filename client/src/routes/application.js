cc7c0b67b3e7b77e8e6287d4da64fcb210ec797c
Ale12345689 <ale12@mit.edu>
2025-01-30 15:02:27 +0100

    Updated Dates and Times

--- client/src/routes/application.js
+++ client/src/routes/application.js
@@ -1,48 +1,46 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router';
+import React from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
 
-import Header from '../components/headers/Header';
-import ApplicationForm from '../components/forms/ApplicationForm';
+import Header from "../components/headers/Header";
+import ApplicationForm from "../components/forms/ApplicationForm";
 
 export default function Application() {
-    const [isLoading, setIsLoading] = useState(true);
-    const [userData, setUserData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [userData, setUserData] = useState({});
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        fetch("/api/me").then(
-            (response) => {
-                if (response.ok) {
-                    response.json().then(data => {
-                        setUserData(data.data);
-                        setIsLoading(false);
-                    })
-                }
-                else {
-                    navigate("/login")
-                }
-            }
-        )
-    }, [navigate]);
+  useEffect(() => {
+    fetch("/api/me").then((response) => {
+      if (response.ok) {
+        response.json().then((data) => {
+          setUserData(data.data);
+          setIsLoading(false);
+        });
+      } else {
+        navigate("/login");
+      }
+    });
+  }, [navigate]);
 
-    return (
-        isLoading ? <div></div> :
-            <div className="bg-gray-50">
-                <div>
-                    <Header firstname={userData.firstname} usertype={userData.usertype} />
-                </div>
-                <div className="flex flex-col items-center justify-center py-8 ">
-                    <div className="w-full bg-white rounded-lg shadow  md:mt-0 sm:max-w-2xl lg:max-w-4xl xl:p-0 ">
-                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                            <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-                                MCG Spring 2024 Application
-                            </h1>
-                            <ApplicationForm />
-                        </div>
-                    </div>
-                </div>
-            </div>
-    )
+  return isLoading ? (
+    <div></div>
+  ) : (
+    <div className="bg-gray-50">
+      <div>
+        <Header firstname={userData.firstname} usertype={userData.usertype} />
+      </div>
+      <div className="flex flex-col items-center justify-center py-8 ">
+        <div className="w-full bg-white rounded-lg shadow  md:mt-0 sm:max-w-2xl lg:max-w-4xl xl:p-0 ">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+              MCG Spring 2025 Application
+            </h1>
+            <ApplicationForm />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
